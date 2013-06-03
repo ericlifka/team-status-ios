3aeee4dcb0a8e94f157c09640f06e6700c10e340
oestrella+elifka <oestrella+elifka@rallydev.com>
2013-06-03 11:07:53 -0400

    Intital project

---
+++ RallyTeamStatus/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  RallyTeamStatus
+//
+//  Created by Pairing on 6/3/13.
+//  Copyright (c) 2013 Rally Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
