ae2c1b1f1dfb9dcd047ccbaa65cba40015d82b6c
oestrella+elifka <oestrella+elifka@rallydev.com>
2013-06-03 12:33:09 -0400

    New Project with storyboard

---
+++ RallyTeamStatus/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RallyTeamStatus
+//
+//  Created by Pairing on 6/3/13.
+//  Copyright (c) 2013 Rally Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RallyTeamStatusTests/RallyTeamStatusTests.h
@@ -0,0 +1,13 @@
+//
+//  RallyTeamStatusTests.h
+//  RallyTeamStatusTests
+//
+//  Created by Pairing on 6/3/13.
+//  Copyright (c) 2013 Rally Software. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RallyTeamStatusTests : SenTestCase
+
+@end
